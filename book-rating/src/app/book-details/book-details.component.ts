7f6b514210a9f3030ea8564b5c88f53d0937e1e8
Ferdinand Malcher <mail@ferdinand-malcher.de>
2017-11-09 10:04:32 +0100

    Routenparameter auslesen

--- book-rating/src/app/book-details/book-details.component.ts
+++ book-rating/src/app/book-details/book-details.component.ts
@@ -1,3 +1,7 @@
+import { map, pluck, switchMap } from 'rxjs/operators';
+import { Book } from './../shared/book';
+import { BookStoreService } from '../shared/book-store.service';
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookDetailsComponent implements OnInit {
 
-  constructor() { }
+  book: Book;
+
+  constructor(
+    private route: ActivatedRoute,
+    private bs: BookStoreService
+  ) { }
 
   ngOnInit() {
+    /*this.route.params.subscribe(p => {
+      this.bs.getSingle(p.isbn).subscribe(b => this.book = b);
+    });*/
+    this.route.params.pipe(
+      pluck('isbn'), // Alternative: map(p => p.isbn)
+      switchMap((isbn: string) => this.bs.getSingle(isbn))
+    )
+    .subscribe(b => this.book = b);
+
+    // Alternative: this.isbn = this.route.snapshot.params.isbn;
   }
 
 }
